e0f2d7c61cf8b385fb3b95c4fbc5b2c09f1b1e4f
dakom35 <123820119+dakom35@users.noreply.github.com>
2023-11-23 16:30:00 +0100

    added isMousePosInRect

--- Game.cpp
+++ Game.cpp
@@ -90,6 +90,37 @@ void Game::pollEvents()
 
 }
 
+bool Game::isMousePosInRect(sf::Vector2f rectPos,sf::Vector2f mousePos,sf::Vector2f rectSize,sf::Vector2f scale)
+{
+    /*
+    *   @return bool inclusion : true if mouse's position is in the rectangle
+
+        @args 
+                sf::Vector2i rectPos : origin point of rectangle 
+                sf::Vector2i mousePos : mouse position
+                sf::Vector2f rectSize : rectangle's size 
+                sf::Vector2f scale : scaling factor
+
+        @variables
+                float rectSizeLengthX : length of rectangle along X axis
+                float rectSizeLengthY : length of rectangle along Y axis
+
+        @brief 
+                a point M is in a rectangle of origin point A if : 
+                    xA <= xM <= xA + xL
+                    yA <= yM <= yA + yL 
+                with xL being the length of rectangle along X axis
+                and yL for the Y axis.
+    */
+    bool inclusion = false ; // answer to question
+    float rectSizeLengthX = rectSize.x * scale.x ;
+    float rectSizeLengthY = rectSize.y * scale.y ;
+    if(rectPos.x <= mousePos.x <= rectPos.x + rectSizeLengthX 
+    && rectPos.y <= mousePos.y <= rectPos.y + rectSizeLengthY ) // see @brief
+        inclusion = true ;
+    return inclusion ;
+}
+
 void Game::updateMousePositions()
 {
     /*
@@ -97,15 +128,29 @@ void Game::updateMousePositions()
         updates the mouse positions :
             - Mouse position relative to window (Vector2i)
     */
+   bool inclusion = false ;
 
    this->mousePosWindow = sf::Mouse::getPosition(*this->window);
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) 
+        {
+            // Get the current mouse position
+            sf::Vector2i mousePos = sf::Mouse::getPosition(*this->window);
+            sf::Vector2f convertedMousePos(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y));
+            inclusion = Game::isMousePosInRect(this->enemy.getPosition(),convertedMousePos,this->enemy.getSize(),this->enemy.getScale());
+       }
    std::cout << "Mouse pos :" << this->mousePosWindow.x << " " << this->mousePosWindow.y << "\n" ;
-}
+   if(inclusion == true) 
+    {
+        std::cout << "Ennemy shot !" << "\n" ;
+    }
+        
+   }
 
 void Game::update()
 {
     this->pollEvents(); 
     this->updateMousePositions();
+
 }
 
 void Game::render()
