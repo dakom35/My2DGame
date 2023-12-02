43b785fb3cc85a6fbc23d93eadb4e17f03907222
dakom35 <alfred.lalanne@gmail.com>
2023-12-02 21:52:50 +0100

    removed commented code

--- main.cpp
+++ main.cpp
@@ -13,29 +13,11 @@ int main()
         - prepare next frame (update) 
         - display it (render)     
     */
-    // std::chrono::high_resolution_clock::time_point start;
-    // std::chrono::high_resolution_clock::time_point end;
-    // int counter = 0 ;
-    // int N = 100 ;
 
-    float fps,avg_fps=0;
     while(game.running())
     {
-        // start = std::chrono::high_resolution_clock::now(); 
-        // Update 
         game.update();
-
-        // Render 
         game.render();
-        // end = std::chrono::high_resolution_clock::now();
-        // fps = (float)1e9/(float)std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count();
-        // avg_fps += fps/N ;
-        // if(counter == N){
-        //     std::cout << "Average FPS = " << avg_fps << "\n";
-        //     counter=0;
-        //     avg_fps = 0.f ;
-        // }
-        // counter++;
     }
 
     // End of application 
